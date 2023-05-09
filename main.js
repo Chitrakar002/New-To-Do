4d778842d7fe980a1bbd272839604a562773e045
Chitrakar002 <130827258+Chitrakar002@users.noreply.github.com>
2023-05-09 16:26:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,16 @@ const btn1 = document.getElementById("btn1");
 const item = document.querySelector(".item");
 const box2 = document.querySelector(".box2");
 const allItems = [];
+function todo(itemName){
+    const temp ={};
+    temp.itemName = itemName;
+    return temp;
+}
 function newData(){
+    if(textbox.value===' ') return;
     allItems.push(todo(textbox.value));
     console.log(allItems);
+    box2.innerHTML = " ";
 
     allItems.forEach((todo)=>{
         box2.innerHTML += `<div class="item"><h2>${todo.itemName}</h2>
@@ -14,5 +21,7 @@ function newData(){
     
 
     })
+    textbox.value = " ";
+
 }
 btn1.addEventListener('click',newData);
