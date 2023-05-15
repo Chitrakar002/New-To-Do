445f306523e0902155fa826a5d0ced16933706a0
Chitrakar002 <130827258+Chitrakar002@users.noreply.github.com>
2023-05-15 14:40:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,52 @@
-const textbox = document.getElementById("textbox");
-const btn1 = document.getElementById("btn1");
-const item = document.querySelector(".item");
-const box2 = document.querySelector(".box2");
+const textbox = document.getElementById('textbox');
+const add = document.getElementById('btn1');
+const box = document.querySelector('.box2');
+const historyBtn = document.getElementById('btn3');
+const box3 = document.querySelector('.box3');
 const allItems = [];
-function todo(itemName){
-    const temp ={};
-    temp.itemName = itemName;
+const historyItem = [];
+ // array create for store 
+// elements
+function toDoLoader(newItem){ // function declaration
+    const temp = {}; // object creation
+    temp.newItem =  newItem; // store newItem as Object
     return temp;
 }
+function arrange(){
+allItems.forEach((toDoLoader)=>{
+    box.innerHTML += `<div class="item"><h2>${toDoLoader.newItem}</h2>
+    <input type="button" value ="Done" class="btn2">
+    </div>`;
+})
+}
 function newData(){
-    if(textbox.value===' ') return;
-    allItems.push(todo(textbox.value));
-    console.log(allItems);
-    box2.innerHTML = " ";
-
-    allItems.forEach((todo)=>{
-        box2.innerHTML += `<div class="item"><h2>${todo.itemName}</h2>
-        <input type="button" value="Done" id="btn2">
-        </div>`
-    
-
-    })
+    if(textbox.value === ' ') return;
+    allItems.push(toDoLoader(textbox.value));
+    box.innerHTML= "  ";
+    arrange();
+    done();
     textbox.value = " ";
 
+    // console(allItems);
+    
 }
-btn1.addEventListener('click',newData);
+function done(){
+    const doneBtn = document.querySelectorAll('.btn2');
+    doneBtn.forEach((ele,index)=>{
+        ele.addEventListener('click',()=>{
+           historyItem.push(allItems.splice(index,1));
+            // console.log(index);
+            console.log(historyItem);
+            box.innerHTML=" ";
+            arrange();
+            done();
+        })
+    });
+}
+add.addEventListener('click',newData);
+// function deleteItem(){
+//     allItems.forEach((toDoLoader)=>{
+//         box.innerHTML += `<div class="delItem"><h2>${toDoLoader.newItem}</h2>
+//         </div>`;
+//     })
+// history.addEventListener('click',deleteItem);
